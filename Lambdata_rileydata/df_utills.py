cef0a31da705f03a9640f92b1a63cbc1e6b28622
fuse999 <rileypence4@gmail.com>
2019-10-30 10:44:57 -0700

    morning update

--- Lambdata_rileydata/df_utills.py
+++ Lambdata_rileydata/df_utills.py
@@ -10,37 +10,41 @@ class Df_Preper:
     Pandas dataframe Cleaning tools
     """
 
+    def __init__(self, df):
+      self.dataframe = df
+
+
     def getcsv(data_url):
         return(pd.read_csv(data_url))  # This is a test function for myself.
 
-    def display_num(df):
+    def display_num(self):
       """
       This function Shows all # with a describe function
       """
-        return(df.select_dtypes(include='number').describe().T)
+        return(self.df.select_dtypes(include='number').describe().T)
 
-    def display_non_num(df):
+    def display_non_num(self):
       """
       This function shows all non numerical columns
       from dataframe in order by unique values
       """
-        return(df.select_dtypes(exclude='number')
+        return(self.df.select_dtypes(exclude='number')
                  .describe().T.sort_values(by='unique'))
 
-    def split_df(df):
+    def split_df(self):
       """
       easy preset train test split
       """
-        train, test = train_test_split(df, test_size=0.30, random_state=59)
+        train, test = train_test_split(self.df, test_size=0.30, random_state=59)
         print("split_df has returned 'train' and 'test' dataframes.")
         return(train, test)
 
-    def remove_outlier(df_in, col_name):
-        q1 = df_in[col_name].quantile(0.25)
-        q3 = df_in[col_name].quantile(0.75)
+    def remove_outlier(self, col_name):
+        q1 = self.df[col_name].quantile(0.25)
+        q3 = self.df[col_name].quantile(0.75)
         iqr = q3-q1  # Interquartile range
         fence_low = q1-1.5*iqr
         fence_high = q3+1.5*iqr
-        df_out = df_in.loc[(df_in[col_name] > fence_low) &
-                           (df_in[col_name] < fence_high)]
+        df_out = self.df.loc[(self.df[col_name] > fence_low) &
+                           (self.df[col_name] < fence_high)]
         return(df_out)
